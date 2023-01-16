f21f4a4c9152871225925b88fc557a650ca2bf9a
cedric-charlesia <ccharlesia@gmail.com>
2023-01-16 16:27:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kent-back-ofice-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
